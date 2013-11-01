9260ac7ef0295cf86cc180c114fc847b4b98d4e2
namih <fatal.experience@gmail.com>
2013-11-01 16:13:22 -0600

    modif codigo

--- mimi.py
+++ mimi.py
@@ -3,34 +3,35 @@
 
 import random
 from libj import *
-
-
-m=10
-min=1
-max=1
-#calcula factorial como limite superior para generar los m cromosomas de la generacion
-def factorial(max,n):  
-	if(n>0):  
-		max=factorial(max,n-1)  
+n=4
+def factorial(n):
+	max=1 
+	if(n>0): 
+		max=factorial(n-1)  
 		max=max*n  
 	else:  
-		max=1  
-	return max  
+		max=1 
+	return max 
 
+def elementosAleatorios(cantidad):
+	pobla=generarPoblacion(cantidad)
+	numeros = set()
+	min=1
+	max=factorial(cantidad)
+	if(max < min):
+		print"error"
+		min, max = max, min
+	if(cantidad > (max-min)):
+		print "error: solicita mas elementos de los que tiene"
+		cantidad = max - min
+	while len(numeros) < cantidad:
+		numeros.add(random.randint(min, max))
+	for i in numeros:
+		print i
+		print pobla[i]
+	return numeros
 
-#Descripción: funcion para elegir los elementos de la primera generacion sin repeticion
-#Parametros: int m es el numero de elementos, int min el limite inferior e int max el limite superior.
-def aleatorios(cantidad, min, max):
-	#print "hola"
-    numeros = set()
-    if(max < min):
-        min, max = max, min
-    #valida que no se pidan mas elementos del rango de seleccion   
-    if(cantidad > (max-min)):
-        cantidad = max - min
-    while len(numeros) < cantidad:
-        numeros.add(random.randint(min, max))
-    return numeros
+print elementosAleatorios (n)
 
 
 #funcion genera numeros aleatorios [0,1]
