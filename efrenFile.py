5379301333c59a6415863d05f1fe6059ac7b76a1
Eigen-13 <efren@ubuntu.(none)>
2013-11-01 11:45:06 -0600

    creacion de efrenFile.py en donde se iran creando las funciones que necesite, contiene la funcion para calcular la aptitud de un cromosoma

--- efrenFile.py
+++ efrenFile.py
@@ -5,8 +5,8 @@ import string
 #Parametros: [lista cromosoma, int n, arreglo 2D matrizA, arreglo 2D matrizB]
 def calcularAptitud(cromosoma, n, matrizA, matrizB):
 	#Definicion del diccionario que usare para saber las posiciones de la permutacion
-	alfabeto = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":6, "H":6, "I":6, "J":6, "K":6, "L":6, "M":6, "N":6, "O":6, "P":6,
-	 "Q":6, "R":6, "S":6, "T":6, "U":6, "V":6, "W":6, "X":6, "Y":6,resultado }
+	alfabeto = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8, "I":9, "J":10, "K":11, "L":12, "M":13, "N":14, "O":15, "P":16,
+	 "Q":17, "R":18, "S":19, "T":20, "U":21, "V":22, "W":23, "X":24, "Y":25, "Z":26}
 	 #en esta variable se guardara el resultado de la aptitud del cromosoma
 	 resultadoAptitud = 0
 	 #Recorro cada elemeto de la matriz a para multiplicarlo con la permutacion del cromosoma en b
