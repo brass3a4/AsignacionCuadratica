b1ddb8480842fd37ee7bd0d2688106b9323b8873
Shangueili <shangueili@Betinorama.(none)>
2013-11-01 14:48:54 -0600

    funcion Cargar Matriz

---
+++ beatriz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python	
+# -*- coding: utf-8 -*-
+
+
+#archivo_matriz_A= "/home/shangueili/Escritorio/algoritmos_proyecto/matrizA.txt"
+#archivo_matriz_B= "/home/shangueili/Escritorio/algoritmos_proyecto/matrizB.txt"
+
+def CargarMatriz(archivo):
+	f = open(archivo)
+	data = f.read().strip()
+	f.close
+	M= [[int(num) for num in line.strip().split()] for line in data.split('\n')]
+	print M
+	
+		
+#print CargarMatriz(archivo_matriz_A)
+#print CargarMatriz(archivo_matriz_B)
