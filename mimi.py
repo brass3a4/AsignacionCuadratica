73e4f4690d1bf919bb914fbf9d8478d4da9e7596
namih <fatal.experience@gmail.com>
2013-11-01 14:19:29 -0600

    correccion identacion

--- mimi.py
+++ mimi.py
@@ -31,3 +31,20 @@ def aleatorios(cantidad, min, max):
     return numeros
 
 
+#funcion genera numeros aleatorios [0,1]
+#funcion: Zi=(a*Zi-1) mod m
+#parametros: modulo m, el multiplicador a y la semilla o valor de comienzo Z0 son enteros no negativos
+#se verifica que:0 menor o igual que Zi menor que m
+#Para obtener un numero aleatorio de la distribucion uniforme [0,1) se debe hacer Ui = Zi/m. Además de ser no negativos se debe veriﬁcar que: 0 < m, a < m, c < m, Z0 < m
+def generaAleatDec(m):
+	Z={}
+	U={}
+	denom= (pow(2,31))-1.0
+	a = 630360016
+	Z[0] = 7
+	for i in range(1, m+1):
+		Z[i]=(a*Z[i-1]) % denom
+	for i in range (1,m+1):
+		U[i]=round((Z[i]/denom),3)
+	U[0]=round(Z[0]/denom,3)
+
