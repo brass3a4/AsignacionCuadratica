f648c060d0052146e4b1a17c102469eaac3ad89e
brass3a4 <brass3a4@gmail.com>
2013-11-03 06:44:09 -0600

    Version final

--- genetico.py
+++ genetico.py
@@ -26,9 +26,10 @@ pc = float(sys.argv[3])
 pm = float(sys.argv[4])
 
 # Limpiamos pantalla antes de iniciar
-os.system("clear")
+#os.system("clear")
 
 def principal():
+	bandera = 0
 	aptitudCromosomasMasApto = 0
 	sumaAptitudes = 0
 	cromosomaMasApto = ""
@@ -36,45 +37,86 @@ def principal():
 	poblacion = generarPoblacion(n)
 
 	# Leemos las matrices A y B que están en los ficheros
-	matrizA = cargarMatriz('matrizA.txt')
-	matrizB = cargarMatriz('matrizB.txt')
+	matrizA = cargarMatriz('matrizA2.txt')
+	matrizB = cargarMatriz('matrizB2.txt')
 	# Tomamos la generación cero de la población total
 	generacion = tomarGeneracion(poblacion,cardinalidadGeneracion)
+	#print "generacion"
+	original = generacion [:]
+	#print generacion
 
+	for iteracion in xrange(1,100):
+		if bandera > 0:
+			generacion = siguienteGeneracion[:]
 
-	aptitudesGeneracion = []
+		aptitudesGeneracion = []
+		
+		# Calculamos la aptitud de cada cromosoma
+		for cromosoma in generacion:
+			aptitudCromosomaAnalizado = calcularAptitud(cromosoma, n, matrizA, matrizB)
+			aptitudesGeneracion.append(aptitudCromosomaAnalizado)
+			if aptitudCromosomaAnalizado < aptitudCromosomasMasApto:
+				aptitudCromosomasMasApto = aptitudCromosomaAnalizado
+				cromosomaMasApto = cromosoma
+			sumaAptitudes = sumaAptitudes + aptitudCromosomaAnalizado
+		probabilidadesUnitarias = calcularProbabilidadUnitaria(aptitudesGeneracion, sumaAptitudes)
+		probabilidadesAcumuladas = calcularProbabilidadAcumulada(probabilidadesUnitarias)
+		
+		cromosomasRuleta = ruleta(generacion,probabilidadesAcumuladas,cardinalidadGeneracion)
+		#print "cromosomasRuleta"
+		#print cromosomasRuleta
+		cromosomasACruzar = traerElementosCruce(cromosomasRuleta,pc,cardinalidadGeneracion) 
+		#print "cromosomas a cruzar"
+		#print cromosomasACruzar
+		nuevosCromosomas =cruzarCromosomas(cromosomasACruzar,n,matrizA,matrizB)
+		#print nuevosCromosomas
+
+		for salen in nuevosCromosomas['salen']:
+			if (salen in generacion):
+				generacion.remove(salen)
+		for entran in nuevosCromosomas['entran']:
+			generacion.append(entran)
+		#print "Nueva Generacion"
+		#print generacion
+		
+		#nuevosCromosomas = generacion
+		#cromosomasMutados = mutar(nuevosCromosomas,pm)
+		#print cromosomasMutados
+		
+
+		aptitudesNuevaGeneracion = []
+		sumaAptitudesNuevas = 0
+		
+		siguienteGeneracion = mutarGeneracionCruzada(generacion, pm, n)
+		bandera = bandera + 1
+		for nuevoCromosoma in siguienteGeneracion:
+			aptitudNuevoCromosomaAnalizado = calcularAptitud(nuevoCromosoma, n, matrizA, matrizB)
+			aptitudesNuevaGeneracion.append(aptitudNuevoCromosomaAnalizado)
+			if aptitudNuevoCromosomaAnalizado < aptitudCromosomasMasApto:
+				aptitudCromosomasMasApto = aptitudNuevoCromosomaAnalizado
+				cromosomaMasApto = nuevoCromosoma
+			sumaAptitudesNuevas = sumaAptitudesNuevas + aptitudNuevoCromosomaAnalizado
+		print "|"
+		os.system("clear")
+		print "-"
+		os.system("clear")
 	
-	# Calculamos la aptitud de cada cromosoma
+	print "Aptitud inicial:",sumaAptitudes
+	print "Aptitud final:",sumaAptitudesNuevas
+	if sumaAptitudesNuevas < sumaAptitudes:
+		print "Mejoro con respecto la generacion anterior"
+	else:
+		print "No mejoro con respecto a la generacion anterior"
+	print "***************"
+	print "Generiacion 0"
+	for cromosoma in original:
+		print cromosoma
+	print "Generacion despues de la cruza"
 	for cromosoma in generacion:
-		aptitudCromosomaAnalizado = calcularAptitud(cromosoma, n, matrizA, matrizB)
-		aptitudesGeneracion.append(aptitudCromosomaAnalizado)
-		if aptitudCromosomaAnalizado < aptitudCromosomasMasApto:
-			aptitudCromosomasMasApto = aptitudCromosomaAnalizado
-			cromosomaMasApto = cromosoma
-		sumaAptitudes = sumaAptitudes + aptitudCromosomaAnalizado
-	probabilidadesUnitarias = calcularProbabilidadUnitaria(aptitudesGeneracion, sumaAptitudes)
-	probabilidadesAcumuladas = calcularProbabilidadAcumulada(probabilidadesUnitarias)
-	print "generacion"
-	print generacion
-	cromosomasRuleta = ruleta(generacion,probabilidadesAcumuladas,cardinalidadGeneracion)
-	print "cromosomasRuleta"
-	print cromosomasRuleta
-	cromosomasACruzar = traerElementosCruce(cromosomasRuleta,pc,cardinalidadGeneracion) 
-	print "cromosomas a cruzar"
-	print cromosomasACruzar
-	nuevosCromosomas =cruzarCromosomas(cromosomasACruzar,n,matrizA,matrizB)
-
-	print nuevosCromosomas
-
-	for salen in nuevosCromosomas['salen']:
-		if (salen in generacion):
-			generacion.remove(salen)
-	for entran in nuevosCromosomas['entran']:
-		generacion.append(entran)
-	print "Nueva Generacion"
-	print generacion
-	#print nuevosCromosomas
-	#cromosomasMutados = mutar(nuevosCromosomas,pm)
-	#print cromosomasMutados
+		print cromosoma
+	print "Ultima generacion"
+	for cromosoma in siguienteGeneracion:
+		print cromosoma
+	print bandera
 
 principal()
--- libj.py
+++ libj.py
@@ -25,7 +25,7 @@ def generaAleatDec(m):
 def generarPoblacion(n):
 
 	#Definicion del diccionario que usare para saber las posiciones de la permutación
-	alfabeto = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 15:"O", 16:"P",17:"Q", 19:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"}
+	alfabeto = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 15:"O", 16:"P",17:"Q", 18:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"}
 	lista = []
 
 	# Creo una lista de n elementos a permutar
@@ -128,6 +128,9 @@ def cruzarCromosomas(cromosomas,n,matrizA,matrizB):
 					entran.append(hijo)
 
 	nuevosCromosomas = {"entran":entran, "salen": salen}
+
+	if(len(nuevosCromosomas['salen']) > len(nuevosCromosomas['entran'])):
+		nuevosCromosomas['salen'].pop()
 
 	return nuevosCromosomas
 
